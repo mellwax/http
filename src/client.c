ac7f3eacbf2cfdfc9c19948a91cbb107e11ae273
Kristijan Todorovic <e11806442@student.tuwien.ac.at>
2023-12-30 13:50:03 +0100

    finished client, all test cases successful except 12_save-to-file-4

--- src/client.c
+++ src/client.c
@@ -12,26 +12,55 @@
 #include <sys/socket.h>
 #include <netdb.h>
 #include <ctype.h>
+#include <errno.h>
 
 #define URL_DELIMS ";/?:@=&"
 
 static const char *program_name;
 
+/**
+ * @brief prints the error message to stderr and terminates the program with EXIT_FAILURE
+ *
+ * @param message error message to be printed
+ */
 static void ERROR_EXIT(const char *message) {
     fprintf(stderr, "[%s]: %s\n", program_name, message);
     exit(EXIT_FAILURE);
 }
 
+/**
+ * @brief prints a usage message to stderr and terminates the program with EXIT_FAILURE
+ *
+ * @param message detailed message describing wrong usage
+ */
 static void USAGE(const char *message) {
     fprintf(stderr, "[%s]: %s\nUSAGE: %s [-p PORT] [ -o FILE | -d DIR ] URL\n", program_name, message, program_name);
     exit(EXIT_FAILURE);
 }
 
+/**
+ * @brief this function frees the url_path and host strings, which were malloced
+ *
+ * @param url_path malloced string to be freed
+ * @param host malloced string to be freed
+ */
 static void free_strings(char *url_path, char *host) {
     free(url_path);
     free(host);
 }
 
+/**
+ * @brief
+ *
+ * @details
+ *
+ * @param argc
+ * @param argv
+ * @param port
+ * @param url_path
+ * @param host
+ * @param output
+ */
 static void parse_args(int argc, char **argv, int *port, char **url_path, char **host, FILE **output) {
     int c;
     int p_count = 0, o_count = 0, d_count = 0;
@@ -90,16 +119,32 @@ static void parse_args(int argc, char **argv, int *port, char **url_path, char *
         free_strings(*url_path, *host);
         USAGE("no url specified");
     }
-    char *url_tmp = argv[optind];
+    char *url_tmp = malloc(sizeof(char) * 300);
+    if (url_tmp == NULL) {
+        free_strings(*url_path, *host);
+        ERROR_EXIT("error allocating memory for string url_tmp in parse_args");
+    }
+    char *url_tmp_ptr = url_tmp;
+    strncpy(url_tmp, argv[optind], 200);
     char *http = "http://";
     int http_len = (int) strlen(http);
 
     if (strncmp(url_tmp, http, http_len) != 0) {
         free_strings(*url_path, *host);
+        free(url_tmp_ptr);
         USAGE("incorrect url");
     }
 
     url_tmp = &url_tmp[http_len];
+
+    for (int i = 0; i <= strlen(URL_DELIMS); i++) {
+        if (url_tmp[0] == URL_DELIMS[i]) {
+            free_strings(*url_path, *host);
+            free(url_tmp_ptr);
+            ERROR_EXIT("invalid hostname");
+        }
+    }
+
     char url_tmp2[300];
     strcpy(url_tmp2, url_tmp);
 
@@ -124,22 +169,40 @@ static void parse_args(int argc, char **argv, int *port, char **url_path, char *
         FILE *file = fopen(d_opt, "w+");
         if (file == NULL) {
             free_strings(*url_path, *host);
+            free(url_tmp_ptr);
             ERROR_EXIT("error opening file specified by -d");
         }
         *output = file;
     }
-
-    fprintf(stdout, "%s\n", d_opt);
+    free(url_tmp_ptr);
 }
 
-static void send_get_request(char *url_path, char *host, FILE *sockfile) {
+/**
+ * @brief
+ *
+ * @param url_path
+ * @param host
+ * @param sockfile
+ * @return 0 if flushing to sockfile was successful
+ */
+static int send_get_request(char *url_path, char *host, FILE *sockfile) {
     char get_request[512];
     sprintf(get_request, "GET %s HTTP/1.1\r\nHost: %s\r\nConnection: close\r\n\r\n", url_path, host);
 
     fputs(get_request, sockfile);
-    fflush(sockfile);
+    return fflush(sockfile);
 }
 
+/**
+ * @brief
+ *
+ * @param ai
+ * @param output
+ * @param sockfile
+ * @param url_path
+ * @param host
+ * @param response
+ */
 static void close_free_resources(struct addrinfo *ai, FILE *output, FILE *sockfile, char *url_path,
         char *host, char *response) {
     freeaddrinfo(ai);
@@ -156,6 +219,18 @@ static void close_free_resources(struct addrinfo *ai, FILE *output, FILE *sockfi
     }
 }
 
+/**
+ *
+ * @param argc
+ * @param argv
+ * @return  EXIT_SUCCESS, if no errors occurred during execution
+ *          <p>
+ *          2, if the HTTP response header is invalid
+ *          <p>
+ *          3, if the HTTP response status code is not 200
+ *          <p>
+ *          EXIT_FAILURE, if any other error occurred during execution
+ */
 int main(int argc, char **argv) {
     program_name = argv[0];
     int port = 80;
@@ -186,6 +261,7 @@ int main(int argc, char **argv) {
 
     if (res != 0) {
         free_strings(url_path, host);
+        freeaddrinfo(ai);
 
         ERROR_EXIT("error getting address info");
     }
@@ -213,10 +289,14 @@ int main(int argc, char **argv) {
         ERROR_EXIT("error connecting to socket");
     }
 
-    send_get_request(url_path, host, sockfile);
+    if (send_get_request(url_path, host, sockfile) != 0) {
+        close_free_resources(ai, output, sockfile, url_path, host, NULL);
+        ERROR_EXIT("error while flushing get request");
+    }
 
-    char buffer[1024];
+    char buffer[2048];
     char *response = malloc(sizeof(char) * 2048);
+    strcpy(response, "");
     size_t response_size = 0;
     size_t response_cap = sizeof(char) * 2048;
 
@@ -234,6 +314,7 @@ int main(int argc, char **argv) {
                 char *ptr = &copy[strlen(http_signature)];
 
                 status_code = (int) strtol(ptr, &status_descr, 10);
+                status_descr = &status_descr[1];
                 if (status_code != 200) {
                     break;
                 }
@@ -261,20 +342,18 @@ int main(int argc, char **argv) {
         strcat(response, buffer);
     }
 
-    fprintf(output, "Request status: %d%s\n", status_code, status_descr);
-
     if (status_code == 200) {
         fputs(response, output);
-        if (fflush(output) == -1) {
+        if (fflush(output) != 0) {
             close_free_resources(ai, output, sockfile, url_path, host, response);
             ERROR_EXIT("error flushing output stream");
         }
-    } else if (status_code == -1) {
+    } else if (status_code == -1 || status_code == 0) {
         fprintf(stderr, "Protocol error!\n");
         close_free_resources(ai, output, sockfile, url_path, host, response);
         return 2;
     } else {
-        fprintf(stderr, "Error %d%s\n", status_code, status_descr);
+        fprintf(stderr, "Error %d %s\n", status_code, status_descr);
         close_free_resources(ai, output, sockfile, url_path, host, response);
         return 3;
     }
